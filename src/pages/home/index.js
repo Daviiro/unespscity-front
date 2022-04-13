d54ccc4e93bf14e0263277cd2d67f04c2a49940a
Pedro H. Z. Costa <67840448+ZagoPedro@users.noreply.github.com>
2022-04-12 22:18:37 -0300

    update 12/04/2022

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,87 @@
 import React from "react";
 
+import { ContainerBase, Header, ContainerRow, ContainerColumn } from './styles'; 
+
+import Logo from '../../assets/home_logo_temporario.png';
+import Configuracoes from '../../assets/home_engrenagem.png';
+import Icon01 from '../../assets/home_conservacao.png';
+import Icon02 from '../../assets/home_saude_vigilancia.png';
+import Icon03 from '../../assets/home_animais_zoonoses.png';
+import Icon04 from '../../assets/home_meio_ambiente.png';
+import Icon05 from '../../assets/home_assistencia_social.png';
+import Icon06 from '../../assets/home_seguranca_defesa.png';
+import Icon07 from '../../assets/home_administracao.png';
+import Icon08 from '../../assets/home_notificacao_comunicacao.png';
+import Icon09 from '../../assets/home_sensoriamento_movel.png';
+import Icon10 from '../../assets/home_associacao_comercial.png';
+import Art from '../../assets/bottom_art.png';
+
 const Home = () => {
 	return (
 		<>
-			<div>
-				<h1>Está vai ser a Homepage</h1>
-			</div>
+			<ContainerBase>
+				<Header>
+					<img src = {Logo} alt = "Logo" />
+					<h1> UnespSCity </h1>
+					<h2> Login </h2>
+					<img src = {Configuracoes} alt = "config" />
+				</Header>
+				<ContainerRow>
+					<ContainerColumn>
+						<img src = {Icon01} alt = "Conservação Pública" />
+						<h4> Conservação Pública </h4>
+						<h5> Itens </h5>
+					</ContainerColumn>
+					<ContainerColumn>
+						<img src = {Icon02} alt = "Saúde e Vigilância Sanitária" />
+						<h4> Saúde e Vigilância Sanitária </h4>
+						<h5> Itens </h5>
+					</ContainerColumn>
+					<ContainerColumn>
+						<img src = {Icon03} alt = "Animais e Zoonoses" />
+						<h4> Animais e Zoonoses </h4>
+						<h5> Itens </h5>
+					</ContainerColumn>
+					<ContainerColumn>
+						<img src = {Icon04} alt = "Meio Ambiente" />
+						<h4> Meio Ambiente </h4>
+						<h5> Itens </h5>
+					</ContainerColumn>
+					<ContainerColumn>
+						<img src = {Icon05} alt = "Assistência Social" />
+						<h4> Assistência Social </h4>
+						<h5> Itens </h5>
+					</ContainerColumn>
+				</ContainerRow>
+				<ContainerRow>
+					<ContainerColumn>
+						<img src = {Icon06} alt = "Segurança e Defesa Civil" />
+						<h4> Segurança e Defesa Civil </h4>
+						<h5> Itens </h5>
+					</ContainerColumn>
+					<ContainerColumn>
+						<img src = {Icon07} alt = "Administração Pública" />
+						<h4> Administração Pública </h4>
+						<h5> Itens </h5>
+					</ContainerColumn>
+					<ContainerColumn>
+						<img src = {Icon08} alt = "Centro de Notificação e Comunicação" />
+						<h4> Centro de Notificação e Comunicação </h4>
+						<h5> Itens </h5>
+					</ContainerColumn>
+					<ContainerColumn>
+						<img src = {Icon09} alt = "Sensoriamento Móvel Participativo" />
+						<h4> Sensoriamento Móvel Participativo </h4>
+						<h5> Itens </h5>
+					</ContainerColumn>
+					<ContainerColumn>
+						<img src = {Icon10} alt = "Assistência Comercial" />
+						<h4> Assistência Comercial </h4>
+						<h5> Itens </h5>
+					</ContainerColumn>
+				</ContainerRow>
+				<img src = {Art} alt = "Page Bottom Art" />
+			</ContainerBase>
 		</>
 	);
 };
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,1 +1,91 @@
 import styled from "styled-components";
+
+export const ContainerBase = styled.div`
+    display: block;
+    justify-content: space-around;
+    height: 100vh;
+
+    img {
+        width: 100%;
+        height: auto;
+        margin: 0;
+        opacity: 0.95;
+    }
+`;
+
+export const ContainerRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 10vh;
+`;
+
+export const ContainerColumn = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    align-items: center;
+
+    img {
+        height: 12.5vh;
+        width: auto;
+        opacity: 1; 
+    }
+
+    img:hover { 
+        -webkit-filter: drop-shadow(0px 0px 15px #3282B8);
+        cursor: pointer; 
+    }
+
+    h4 {
+        margin-bottom: -3vh;
+        color: #1B262C;
+    }
+
+    h5 { color: #3282B8; }
+`;
+
+export const Header = styled.div`
+    height: 12.5vh;
+    width: 95.5vw;
+    background-image: linear-gradient(to right, #0F4C75, #3282B8, #0F4C75);
+    margin: 0;
+    margin-bottom: 15vh;
+    border-top: 1vh solid #133D59;
+    border-bottom: 1vh solid #133D59;
+    display: fixed;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-left: 1.8vw;
+    padding-right: 1.8vw;
+
+    img {
+        height: 6.75vh;
+        width: auto;
+        margin-top: 2.75vh;
+        opacity: 1;
+    }
+
+    img:hover { 
+        -webkit-filter: drop-shadow(0px 0px 10px #FFFFFF);
+        cursor: pointer; 
+    }
+
+    h1 { 
+        color: #FFFFFF;
+        text-shadow: -1.5px 0 black, 0 1.5px black, 1.5px 0 black, 0 -1.5px black;
+        margin-top: 3.5vh; 
+    }
+
+    h2 {
+        color: #FFFFFF;
+        text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+        margin-right: -32vw;
+        margin-top: 4.25vh;
+    }
+
+    h2:hover { 
+        text-shadow:  0 0 15px #FFFFFF;
+        cursor: pointer; 
+    }
+`;
