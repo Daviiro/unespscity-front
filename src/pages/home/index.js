a231dc1694f1849ab3ce0debe93967624baa2c1b
David Jr <david.junior@unesp.br>
2022-04-30 16:24:33 -0300

    arrumei a pasta de importacao das imagens

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,71 +1,104 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { ContainerBase, Header, ContainerRow, ContainerColumn, Square } from './styles'; 
-
-import Logo from '../../assets/home_logo_temporario.png';
-import Configuracoes from '../../assets/home_engrenagem.png';
-import Icon01 from '../../assets/home_conservacao_urbana.png';
-import Icon02 from '../../assets/home_conservacao_rural.png';
-import Icon03 from '../../assets/home_remocao_detritos.png';
-import Icon04 from '../../assets/home_vigilancia_sanitaria.png';
-import Icon05 from '../../assets/home_controle_pragas.png';
-import Icon06 from '../../assets/home_animais_domesticos.png';
-import Icon07 from '../../assets/home_meio_ambiente.png';
-import Icon08 from '../../assets/home_fauna_flora.png';
-import Icon09 from '../../assets/home_assistencia_social.png';
-import Icon10 from '../../assets/home_familias_carentes.png';
-import Icon11 from '../../assets/home_servicos_sociais.png';
-import Icon12 from '../../assets/home_seguranca_defesa_civil.png';
-import Icon13 from '../../assets/home_botao_panico.png';
-import Icon14 from '../../assets/home_administracao_publica.png';
-import Icon15 from '../../assets/home_notificacao_comunicacao.png';
-import Icon16 from '../../assets/home_sensoriamento_movel_participativo.png';
-import Icon17 from '../../assets/home_assossiacao_comercial.png';
-import Art from '../../assets/bottom_art.png';
+import {
+	ContainerBase,
+	Header,
+	ContainerRow,
+	ContainerColumn,
+	Square,
+} from "./styles";
 
 const Home = () => {
 	return (
 		<>
 			<ContainerBase>
 				<Header>
-					<Link to = "/"> <img src = {Logo} alt = "Logo" /> </Link>
+					<Link to="/">
+						{" "}
+						<img
+							src={
+								process.env.PUBLIC_URL +
+								"/assets/img/home_logo_temporario.png"
+							}
+							alt="Logo"
+						/>
+					</Link>
 					<h1> UnespSCity </h1>
-					<Link to = "/login" style = {{ textDecoration: "none" }}> <h2> Login </h2> </Link>
-					<img src = {Configuracoes} alt = "config" />
+					<Link to="/login" style={{ textDecoration: "none" }}>
+						{" "}
+						<h2> Login </h2>{" "}
+					</Link>
+					<img
+						src={
+							process.env.PUBLIC_URL +
+							"/assets/img/home_engrenagem.png"
+						}
+						alt="Logo"
+					/>
 				</Header>
 				<ContainerRow>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon01} alt = "Conservação Urbana" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_conservacao_urbana.png"
+								}
+								alt="Conservação Urbana"
+							/>
 							<h4> Conservação Urbana </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon02} alt = "Conservação Rural/Áreas Verdes" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_conservacao_rural.png"
+								}
+								alt="Conservação Rural"
+							/>
 							<h4> Conservação Rural/Áreas Verdes </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon03} alt = "Remoção de Detritos" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_remocao_detritos.png"
+								}
+								alt="Remoção de Detritos"
+							/>
 							<h4> Remoção de Detritos </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon04} alt = "Vigilância Sanitária" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_vigilancia_sanitaria.png"
+								}
+								alt="Vigilância Sanitária"
+							/>
 							<h4> Vigilância Sanitária </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon05} alt = "Controle de Pragas" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_controle_pragas.png"
+								}
+								alt="Controle de Pragas"
+							/>
 							<h4> Controle de Pragas </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
@@ -74,35 +107,65 @@ const Home = () => {
 				<ContainerRow>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon06} alt = "Animais Domésticos" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_animais_domesticos.png"
+								}
+								alt="Animais Domésticos"
+							/>
 							<h4> Animais Domésticos </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon07} alt = "Meio Ambiente" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_meio_ambiente.png"
+								}
+								alt="Meio Ambiente"
+							/>
 							<h4> Meio Ambiente </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon08} alt = "Fauna e Flora" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_fauna_flora.png"
+								}
+								alt="Fauna e Flora"
+							/>
 							<h4> Fauna e Flora </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon09} alt = "Assistência Social" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_assistencia_social.png"
+								}
+								alt="Assistência Social"
+							/>
 							<h4> Assistência Social </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon10} alt = "Famílias Carentes" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_familias_carentes.png"
+								}
+								alt="Famílias Carentes"
+							/>
 							<h4> Famílias Carentes </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
@@ -111,35 +174,65 @@ const Home = () => {
 				<ContainerRow>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon11} alt = "Serviços Sociais" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_servicos_sociais.png"
+								}
+								alt="Serviços Sociais"
+							/>
 							<h4> Serviços Sociais </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon12} alt = "Segurança e Defesa Civil" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_seguranca_defesa_civil.png"
+								}
+								alt="Segurança e Defesa Civil"
+							/>
 							<h4> Segurança e Defesa Civil </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon13} alt = "Botão do Pânico" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_botao_panico.png"
+								}
+								alt="Botão do Pânico"
+							/>
 							<h4> Botão do Pânico </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon14} alt = "Administração Pública" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_administracao_publica.png"
+								}
+								alt="Administração Pública"
+							/>
 							<h4> Administração Pública </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon15} alt = "Central de Notificação e Comunicação" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_notificacao_comunicacao.png"
+								}
+								alt="Central de Notificação e Comunicação"
+							/>
 							<h4> Central de Notificação e Comunicação </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
@@ -148,20 +241,36 @@ const Home = () => {
 				<ContainerRow>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon16} alt = "Sensoriamento Móvel Participativo" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_sensoriamento_movel_participativo.png"
+								}
+								alt="Sensoriamento Móvel Participativo"
+							/>
 							<h4> Sensoriamento Móvel Participativo </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 					<Square>
 						<ContainerColumn>
-							<img src = {Icon17} alt = "Assossiação Comercial" />
+							<img
+								src={
+									process.env.PUBLIC_URL +
+									"/assets/img/home_assossiacao_comercial.png"
+								}
+								alt="Assossiação Comercial"
+							/>
 							<h4> Assossiação Comercial </h4>
 							<h5> Itens </h5>
 						</ContainerColumn>
 					</Square>
 				</ContainerRow>
-				<img src = {Art} alt = "Page Bottom Art" />
+
+				<img
+					src={process.env.PUBLIC_URL + "/assets/img/bottom_art.png"}
+					alt="Page Bottom Art"
+				/>
 			</ContainerBase>
 		</>
 	);
