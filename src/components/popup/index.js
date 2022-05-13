e284c5af73c3b48b0c8d55fc5237606c26fab95c
David Jr <david.junior@unesp.br>
2022-05-13 14:41:43 -0300

    ëxclusao de arquivo css desnecessario para migracao do popup para styled components

--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -1,31 +1,27 @@
 import React, { useState } from "react";
-import { Popup } from "./styles";
-import "./modal.css";
+import { Modal, ModalContent, CloseModal } from "./styles";
 import { Link } from "react-router-dom";
 
 const PopUp = (props) => {
 	console.log(props.linkItems);
-	const teste = "tsete";
+
 	return (
 		<div onClick={props.setTrigger}>
 			{props.trigger && (
-				<div className="modal">
-					<div className="overlay">
-						<div className="modal-content">
-							<button
-								className="close-modal"
-								onClick={props.setTrigger}
-							>
+				<Modal>
+					<Modal className="overlay">
+						<ModalContent>
+							<CloseModal onClick={props.setTrigger}>
 								X
-							</button>
+							</CloseModal>
 							{props.linkItems.map((item) => (
 								<Link to={item.link}>
 									<div>{item.name}</div>
 								</Link>
 							))}
-						</div>
-					</div>
-				</div>
+						</ModalContent>
+					</Modal>
+				</Modal>
 			)}
 		</div>
 	);
