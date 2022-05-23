20cbcd33a60a1d27afb6540b045eed25ed135e12
D-Ono <hiromi.ono@unesp.br>
2022-05-23 13:00:46 -0300

    adicionando Modal

--- src/components/admin-list-card/index.js
+++ src/components/admin-list-card/index.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, ButtonDiv, ContainerColumn, InfoContainer, ListCardContainer } from "./styles";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const AdminListCard = (props) => {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <ListCardContainer>
             <InfoContainer>
@@ -16,15 +31,56 @@ const AdminListCard = (props) => {
                 </ContainerColumn>
             </InfoContainer>
             <ButtonDiv>
-                <Button
-                >
+                <Button variant="outlined" onClick={handleClickOpen}>
                     Editar
                 </Button>
-                <Button
-
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
                 >
+                    <DialogTitle id="alert-dialog-title">
+                    {"Use Google's location service?"}
+                    </DialogTitle>
+                    <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Let Google help apps determine location. This means sending anonymous
+                        location data to Google, even when no apps are running.
+                    </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={handleClose}>Disagree</Button>
+                    <Button onClick={handleClose} autoFocus>
+                        Agree
+                    </Button>
+                    </DialogActions>
+                </Dialog>
+                <Button variant="outlined" onClick={handleClickOpen}>
                     Excluir
                 </Button>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                    {"Use Google's location service?"}
+                    </DialogTitle>
+                    <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Let Google help apps determine location. This means sending anonymous
+                        location data to Google, even when no apps are running.
+                    </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={handleClose}>Disagree</Button>
+                    <Button onClick={handleClose} autoFocus>
+                        Agree
+                    </Button>
+                    </DialogActions>
+                </Dialog>
             </ButtonDiv>
         </ListCardContainer>
     );
