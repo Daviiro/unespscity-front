a44bd094ad911a078048588ce13eb81054295134
Pedro H. Z. Costa <67840448+ZagoPedro@users.noreply.github.com>
2022-07-04 12:44:24 -0300

    Update 04/07/2022

--- src/services/map-page-test/index2.js
+++ src/services/map-page-test/index2.js
@@ -6,12 +6,20 @@ const containerStyle = {
   height: "75vh"
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
+let Lat = 0, Lng = 0;
+
+navigator.geolocation.getCurrentPosition((location) => {
+  Lat = location.coords.latitude;  //-23.5557714 (coordenadas do Inova, como exemplo)
+  Lng = location.coords.longitude; //-46.6395571
+});
 
 function MapComponent() {
+
+  const center = {  // calibrando a Latitude e Longitude //
+    lat: (Lat + 1.4217894),
+    lng: (Lng - 4.8080857)
+  };
+
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyBQ7EzutsOQVslr8TE5Zh2s5XKK50Q4Oo8"
@@ -20,6 +28,7 @@ function MapComponent() {
   const [map, setMap] = React.useState(null)
 
   const onLoad = React.useCallback(function callback(map) {
+    console.log(center);
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
     setMap(map)
@@ -32,9 +41,9 @@ function MapComponent() {
   return isLoaded ? (
       <GoogleMap
         mapContainerStyle = {containerStyle}
-        center = {center}
         zoom = {0}
         onLoad = {onLoad}
+        center = {center}
         onUnmount = {onUnmount}
       >
         { /* Child components, such as markers, info windows, etc. */ }
