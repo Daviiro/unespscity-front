57c8ab19574f46616a7291d48c8ef06dbe0e1def
David Ono <david-ono10@outlook.com>
2022-07-22 23:02:33 -0300

    admin adocao animais

--- src/pages/administrator/domestic-animals/adoption-animals/index.js
+++ src/pages/administrator/domestic-animals/adoption-animals/index.js
@@ -1,26 +1,25 @@
-import React from "react";
-
-import {
-	ContainerBase,
-	Details,
-	SubHeader,
-	ContainerColumn,
-	Square,
-} from "./styles";
+import React, { useState } from "react";
 
 import Header from "../../../../components/header";
 import MiniCard from "../../../../components/mini-card";
-import { StyledHr } from "../../../../components/styled-components/StyledHr";
-import ServiceDescription from "../../../../components/service-description";
 import Footer from "../../../../components/footer";
+import Typography from "@mui/material/Typography";
+import {
+	ContainerBase,
+	ContentContainer,
+	TopContentContainer,
+	DescriptionText,
+	MidContentContainer,
+} from "../../../../components/styled-components/PageStyles";
 import AdminListCard from "../../../../components/card-list-admin";
 
 const AdminAdoptionAnimals = () => {
-    return (
-		<>
-			<ContainerBase>
-				<Header />
-				<SubHeader>
+    
+	return (
+		<ContainerBase>
+			<Header />
+			<ContentContainer>
+				<TopContentContainer>
 					<MiniCard
 						source="/assets/img/home_animais_domesticos.png"
 						titulo="Animais Domésticos"
@@ -52,44 +51,59 @@ const AdminAdoptionAnimals = () => {
 							},
 						]}
 					/>
-					<ContainerColumn>
-						<h1> Animais Abandonados </h1>
-						<StyledHr />
-					</ContainerColumn>
-				</SubHeader>
-				<Square>
-					<ServiceDescription description="Lista de todas as notificações sobre animais Abandonados na rua." />
-					<Details>
-						<AdminListCard
-							source="/assets/img/home_assistencia_social.png"
-							nome="Nome"
-							sobrenome="Telefone"
-							click="showDeleteConfirm"
-							descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet ipsum dolor. Vivamus imperdiet semper odio sed consequat. Praesent cursus dui a porta blandit. Aliquam erat volutpat. Morbi quis ex sapien. Aliquam efficitur lorem mattis, vehicula justo sed, porta mi. Nulla at pulvinar ligula, eu dapibus felis. Cras vel orci eu dolor hendrerit dictum aliquet sed orci. Aliquam ultricies dignissim diam ut ornare."
-						/>
-						<AdminListCard
-							source="/assets/img/home_assistencia_social.png"
-							nome="Nome"
-							sobrenome="Telefone"
-							descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet ipsum dolor. Vivamus imperdiet semper odio sed consequat. Praesent cursus dui a porta blandit. Aliquam erat volutpat. Morbi quis ex sapien. Aliquam efficitur lorem mattis, vehicula justo sed, porta mi. Nulla at pulvinar ligula, eu dapibus felis. Cras vel orci eu dolor hendrerit dictum aliquet sed orci. Aliquam ultricies dignissim diam ut ornare."
-						/>
-						<AdminListCard
-							source="/assets/img/home_assistencia_social.png"
-							nome="Nome"
-							sobrenome="Telefone"
-							descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet ipsum dolor. Vivamus imperdiet semper odio sed consequat. Praesent cursus dui a porta blandit. Aliquam erat volutpat. Morbi quis ex sapien. Aliquam efficitur lorem mattis, vehicula justo sed, porta mi. Nulla at pulvinar ligula, eu dapibus felis. Cras vel orci eu dolor hendrerit dictum aliquet sed orci. Aliquam ultricies dignissim diam ut ornare."
-						/>
-						<AdminListCard
-							source="/assets/img/home_assistencia_social.png"
-							nome="Nome"
-							sobrenome="Telefone"
-							descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet ipsum dolor. Vivamus imperdiet semper odio sed consequat. Praesent cursus dui a porta blandit. Aliquam erat volutpat. Morbi quis ex sapien. Aliquam efficitur lorem mattis, vehicula justo sed, porta mi. Nulla at pulvinar ligula, eu dapibus felis. Cras vel orci eu dolor hendrerit dictum aliquet sed orci. Aliquam ultricies dignissim diam ut ornare."
-						/>
-					</Details>
-				</Square>
-				<Footer />
-			</ContainerBase>
-		</>
+					<div style={{ marginTop: "14px" }}>
+						<div style={{ textAlign: "center" }}>
+							<Typography variant="h4">
+								Doação de Animais 
+							</Typography>
+						</div>
+						<DescriptionText>
+							Lista de todas as notificações sobre doações de animais 
+							feitas pelo usuario.
+						</DescriptionText>
+					</div>
+					<div></div>
+				</TopContentContainer>
+				<MidContentContainer>
+					<AdminListCard
+						source="/assets/img/home_fauna_flora.png"
+						nome="Denúncia de Comércio Ilegal"
+						sobrenome="[localização]"
+						descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet ipsum dolor. Vivamus imperdiet semper odio sed consequat. Praesent cursus dui a porta blandit. Aliquam erat volutpat. Morbi quis ex sapien. Aliquam efficitur lorem mattis, vehicula justo sed, porta mi. Nulla at pulvinar ligula, eu dapibus felis. Cras vel orci eu dolor hendrerit dictum aliquet sed orci. Aliquam ultricies dignissim diam ut ornare."
+						report={true}
+					/>
+					<AdminListCard
+						source="/assets/img/home_fauna_flora.png"
+						nome="Ocorrência de animal silvestre preso/perdido/em cativeiro"
+						sobrenome="[localização]"
+						descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet ipsum dolor. Vivamus imperdiet semper odio sed consequat. Praesent cursus dui a porta blandit. Aliquam erat volutpat. Morbi quis ex sapien. Aliquam efficitur lorem mattis, vehicula justo sed, porta mi. Nulla at pulvinar ligula, eu dapibus felis. Cras vel orci eu dolor hendrerit dictum aliquet sed orci. Aliquam ultricies dignissim diam ut ornare."
+						report={true}
+					/>
+					<AdminListCard
+						source="/assets/img/home_fauna_flora.png"
+						nome="Ocorrência de animal silvestre causando perturbação pública"
+						sobrenome="[localização]"
+						descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet ipsum dolor. Vivamus imperdiet semper odio sed consequat. Praesent cursus dui a porta blandit. Aliquam erat volutpat. Morbi quis ex sapien. Aliquam efficitur lorem mattis, vehicula justo sed, porta mi. Nulla at pulvinar ligula, eu dapibus felis. Cras vel orci eu dolor hendrerit dictum aliquet sed orci. Aliquam ultricies dignissim diam ut ornare."
+						report={true}
+					/>
+					<AdminListCard
+						source="/assets/img/home_fauna_flora.png"
+						nome="Denúncia de Comércio Ilegal"
+						sobrenome="[localização]"
+						descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet ipsum dolor. Vivamus imperdiet semper odio sed consequat. Praesent cursus dui a porta blandit. Aliquam erat volutpat. Morbi quis ex sapien. Aliquam efficitur lorem mattis, vehicula justo sed, porta mi. Nulla at pulvinar ligula, eu dapibus felis. Cras vel orci eu dolor hendrerit dictum aliquet sed orci. Aliquam ultricies dignissim diam ut ornare."
+						report={true}
+					/>
+					<AdminListCard
+						source="/assets/img/home_fauna_flora.png"
+						nome="Ocorrência de animal silvestre preso/perdido/em cativeiro"
+						sobrenome="[localização]"
+						descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet ipsum dolor. Vivamus imperdiet semper odio sed consequat. Praesent cursus dui a porta blandit. Aliquam erat volutpat. Morbi quis ex sapien. Aliquam efficitur lorem mattis, vehicula justo sed, porta mi. Nulla at pulvinar ligula, eu dapibus felis. Cras vel orci eu dolor hendrerit dictum aliquet sed orci. Aliquam ultricies dignissim diam ut ornare."
+						report={true}
+					/>
+				</MidContentContainer>
+			</ContentContainer>
+			<Footer />
+		</ContainerBase>
 	);
 }
 
