cec021ea0124a3f1180483d87bdcd6aac6d7c6b2
David Jr <david.junior@unesp.br>
2022-11-10 20:50:07 -0300

    feat: I connected the front notifications with backend

--- src/pages/notifications/index.js
+++ src/pages/notifications/index.js
@@ -9,6 +9,7 @@ import {
 import Footer from "../../components/footer";
 import { Typography } from "@mui/material";
 import NotificationList from "../../components/notificationList";
+import { Card } from "../../components/notificationList/styles";
 import { StyledHr } from "../../components/styled-components/StyledHr";
 import { api } from "../../services/api";
 import { Context } from "../../context/Auth/AuthContext";
@@ -52,10 +53,20 @@ const notificationData2 = [
 			try {
 				const { data } = await api.get("/notify-by-user", {
 					data: {
-						id: user.userId,
+						userId: user.userId,
 					},
 				});
 				setNotificationData(data);
+				//atualizar todas como "lidas"
+				try {
+					api.put("/notify", {
+						userId: user.userId,
+					}).then((res) => {
+						console.log(res);
+					});
+				} catch (e) {
+					console.log(e);
+				}
 			} catch (e) {
 				console.log(e);
 			}
@@ -79,7 +90,48 @@ const notificationData2 = [
 				</TopContentContainer>
 
 				<MidContentContainer>
-					<NotificationList data={notificationData} />
+					{notificationData.length !== 0 ? (
+						notificationData.map((notif) => {
+							if (notif.userId === user.userId) {
+								<Card key={notif._id}>
+									<span>{notif.name}</span>
+									<div
+										style={{
+											display: "flex",
+											flexDirection: "column",
+										}}
+									>
+										<span className="description-style">
+											{notif.description}
+										</span>
+										<br />
+
+										{notif.status === 1 && (
+											<span>Status: Não lido</span>
+										)}
+										{notif.status === 2 && (
+											<span>Status: Lido</span>
+										)}
+									</div>
+									<div>
+										<span>{notif.date.getUTCDate()}</span>/
+										<span>
+											{notif.date.getUTCMonth() + 1}
+										</span>
+										/
+										<span>
+											{notif.date.getUTCFullYear()}
+										</span>
+									</div>
+								</Card>;
+							}
+						})
+					) : (
+						<h4>Você ainda não possui nenhuma notificação</h4>
+					)}
+					{/**
+					 * <NotificationList data={notificationData} />
+					 */}
 				</MidContentContainer>
 			</ContentContainer>
 			<Footer />
